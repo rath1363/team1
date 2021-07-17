3ce92b9c383a6670d310b9a402b18a40b5c0619b
rath1363 <rath1363@colorado.edu>
2021-07-17 14:45:31 -0700

    entertainment table populated

--- test_db.py
+++ test_db.py
@@ -8,6 +8,7 @@ class DBTestCase(unittest.TestCase):
     def setUp(self):
         db.create('test.db')
         db.parseLodging('test.db')
+        db.parseEnt('test.db')
         # print("g")
         self.conn = sqlite3.connect('test.db')
         self.cur = self.conn.cursor()
@@ -22,12 +23,20 @@ class DBTestCase(unittest.TestCase):
         self.conn.close()
         os.remove('test.db')
 
-    def test_category(self):   #, 
+    def test_lodging(self):   #, 
 
         self.cur.execute("select Lodging.lodgingID, name, address, tags from Lodging join Tags on Lodging.lodgingID = Tags.lodgingID")
         rows = self.cur.fetchall()
         for i in rows:
+            # print(i)
+            pass
+
+    def test_ent(self):
+        self.cur.execute("select * from Entertainment as e JOIN Tags as t ON e.entertainmentID = t.entertainmentID")
+        rows = self.cur.fetchall()
+        for i in rows:
             print(i)
+            pass
 
 # Main: Run Test Cases
 if __name__ == '__main__':
