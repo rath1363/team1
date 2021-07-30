3b597a408936578b31baa397c3e77647d588982e
tarapanz <85718613+tarapanz@users.noreply.github.com>
2021-07-30 13:43:24 -0700

    hotel setup and unit tests utilize database

--- hotel_setup.py
+++ hotel_setup.py
@@ -1,26 +1,60 @@
+#!/usr/bin/env python3
+import random
+import sqlite3
+
+
+"""
+This module sets up the Hotel Constructor and the function used to detirmine the pertinant hotel information.
+
+"""
 
 class Hotel:
-    def __init__(self,row):
-        name, rating, cost = row.split(',')
-        self.name = name
-        self.rating = rating
-        self.cost = cost
+    def __init__(self, dbname, max_cost_pref, min_rating_pref):
 
-def pick_hotel(max_price_pref, min_rating_pref):
-    hotel_options = []
-    f=open("raw_hotel_info.txt", 'r')
-    hotel_string= f.readlines()
-    for line in hotel_string:
-        #line = line.rstrip()
-        obj = Hotel(line)
-        if int(obj.rating) >= min_rating_pref:
-            if int(obj.cost) <= max_price_pref:
-                print(obj.name, "is an optimal pick")
-                hotel_options.append(obj.name)            
-    print("The following hotels are your options using the current selection:", hotel_options) 
-    if hotel_options == []:
-        print("No options available at this time")   
-    return hotel_options
-    f.close()
+        if type(max_cost_pref) != int:
+            raise TypeError("Hotel input 'max_price_pref' must be an int")
+
+        if type(min_rating_pref) != int:
+            raise TypeError("Hotel input 'min_rating_pref' must be an int")
+
+        if (type(dbname) != str):
+            raise TypeError("Hotel input 'dbname' must be string")
+
+        if min_rating_pref > 5:
+            raise Exception("Hotel input 'min_rating_pref' must be 5 or lower")
+
+        self.name = "N/A"
+        self.rating_pref = min_rating_pref
+        self.rating = 0
+        self.cost_pref = max_cost_pref
+        self.cost = 0
+        self.address = "N/A"
+        self.phone = 0
+        self.database = dbname
+
+
+    def pick_hotel(self):
+        conn = sqlite3.connect(self.database)
+        curr = conn.cursor()
+
+        query = """SELECT * from Lodging"""
+        curr.execute(query)
+        records = curr.fetchall()
+        shuffled_records = random.sample(records, len(records))
+        for row in shuffled_records:
+            if row[4] >= self.rating_pref:
+                if row[5] <= self.cost_pref:                
+                    self.name = row[1]
+                    self.address = row[2]
+                    self.phone = row[3]
+                    self.rating = row[4]
+                    self.cost = row[5]
+                    conn.commit()
+                    conn.close()
+                    return
+
+        conn.commit()
+        conn.close()
+
 
 
--- hotel_unit_tests.py
+++ hotel_unit_tests.py
@@ -1,50 +1,99 @@
 #!/usr/bin/env python3
 import hotel_setup
-
-def hotel_struct_test():
-    try:
-        print("Unit Testing Item")
-        item = hotel_setup.Hotel("South Beach Plaza Villas,3,86") 
-        if item.name == "South Beach Plaza Villas":
-            print("Name Success")
-        else:
-            print("Name Fail", item.name)
-        if item.rating == "3":
-            print("Rating Success")
-        else:
-            print("Rating Fail", item.rating)
-        if item.cost == "86":
-            print("Cost Success!")
-        else:
-            print("Cost Failure", item.cost)
-    except AttributeError as e:
-        print("Fail")
-        print(e)
+import unittest
 
 
-def list_test_1():
-    max_price_pref = 195
-    min_rating_pref = 4
-    result =  hotel_setup.pick_hotel(max_price_pref, min_rating_pref)
-    if "Gale South Beach" in result:
-        if "Iberostar Berkeley Shore" in result:
-            print ("Success!")
-    else:
-        print("fail")
+class hotelTest(unittest.TestCase):
 
-def list_test_2():
-    max_price_pref = 50
-    min_rating_pref = 1
-    result = hotel_setup.pick_hotel(max_price_pref, min_rating_pref)
-    if result == []:
-        print ("Success!")
-    else:
-        print("fail")
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_init(self):
+        max_cost_pref = 250
+        min_rating_pref = 3
+        dbname = 'lodging_test'
+        item_1 = hotel_setup.Hotel(dbname, max_cost_pref, min_rating_pref) 
+        self.assertEqual(item_1.name, "N/A", "Error in attribute: name" )
+        self.assertEqual(item_1.rating, 0, "Error in attribute: rating" )
+        self.assertEqual(item_1.rating_pref, min_rating_pref, "Error in attribute: rating_pref" )
+        self.assertEqual(item_1.cost, 0, "Error in attribute: cost" )
+        self.assertEqual(item_1.cost_pref, max_cost_pref, "Error in attribute: cost_pref" )
+        self.assertEqual(item_1.address, "N/A", "Error in attribute: address")
+        self.assertEqual(item_1.phone, 0, "Error in attribute: phone")
+        self.assertEqual(item_1.database, 'lodging_test', "Error in attribute: database")
+
+
+    def test_init_cost(self):
+        min_rating_pref = 3
+        str_cost = "hello"
+        flt_cost = 314.15
+        dbname = 'lodging_test'
+
+        with self.assertRaises(TypeError) as e:
+            hotel_setup.Hotel(dbname, str_cost, min_rating_pref) 
+        with self.assertRaises(TypeError) as e:
+            hotel_setup.Hotel(dbname, flt_cost, min_rating_pref)
+
+    def test_init_rating(self):
+        max_cost_pref = 300
+        str_rating = "hello"
+        flt_rating = 314.15
+        high_rating = 6
+        dbname = 'lodging_test'
+
+        with self.assertRaises(TypeError) as e:
+            hotel_setup.Hotel(dbname, max_cost_pref, str_rating) 
+        with self.assertRaises(TypeError) as e:
+            hotel_setup.Hotel(dbname, max_cost_pref, flt_rating)
+        with self.assertRaises(Exception) as e:
+            hotel_setup.Hotel(dbname, max_cost_pref, high_rating)
+
+    def test_db_input(self):
+        max_cost_pref = 300
+        min_rating_pref = 3
+        int_db = 999
+        flt_db = 99.99
+
+        with self.assertRaises(TypeError) as e:
+            hotel_setup.Hotel(int_db, max_cost_pref, min_rating_pref) 
+        with self.assertRaises(TypeError) as e:
+            hotel_setup.Hotel(flt_db, max_cost_pref, min_rating_pref)
+
+    def test_pick_hotel(self):
+        max_cost_pref_1 = 250
+        min_rating_pref_1 = 4
+        dbname = 'lodging_test'
+        item_1 = hotel_setup.Hotel(dbname, max_cost_pref_1, min_rating_pref_1)
+        item_1.pick_hotel()
+        self.assertTrue(item_1.name == "Best Hotel")
+        self.assertTrue(item_1.rating == 5)
+        self.assertTrue(item_1.cost == 200)
+        self.assertTrue(item_1.address == "102 abc Ave, Miami Beach, FL 33139")
+        self.assertTrue(item_1.phone == 2222222222)
+
+        max_cost_pref_2 = 49
+        min_rating_pref_2 = 1
+        item_2 = hotel_setup.Hotel(dbname, max_cost_pref_2, min_rating_pref_2)
+        item_2.pick_hotel
+        self.assertTrue(item_2.name == "N/A")
+        self.assertTrue(item_2.rating == 0)
+        self.assertTrue(item_2.cost == 0)
+        self.assertTrue(item_2.address == "N/A")
+        self.assertTrue(item_2.phone == 0)
 
 
 
-
-hotel_struct_test()
-list_test_1()
-list_test_2()
+if __name__ == '__main__':
+    unittest.main()
 
